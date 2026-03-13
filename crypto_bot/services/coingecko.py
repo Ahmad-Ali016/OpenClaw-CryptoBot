8cfb25ff77e3a300b7b7057a65523f1048c6defe
Ahmad-Ali <mhrali016@gmail.com>
2026-03-14 00:50:53 +0500

    Improved price formatter and added PKR support

--- crypto_bot/services/coingecko.py
+++ crypto_bot/services/coingecko.py
@@ -6,7 +6,7 @@ COINGECKO_URL = "https://api.coingecko.com/api/v3/simple/price"
 def get_crypto_prices():
     params = {
         "ids": "bitcoin,ethereum",
-        "vs_currencies": "usd,usdt"
+        "vs_currencies": "usd,pkr"
     }
 
     response = requests.get(COINGECKO_URL, params=params)
@@ -23,9 +23,11 @@ def format_prices(data):
 
     btc_usd = btc.get("usd", "N/A")
     btc_usdt = btc.get("usdt", "N/A")
+    btc_pkr = btc.get("pkr", "N/A")
 
     eth_usd = eth.get("usd", "N/A")
     eth_usdt = eth.get("usdt", "N/A")
+    eth_pkr = eth.get("pkr", "N/A")
 
     message = f"""
  Crypto Prices
@@ -33,10 +35,12 @@ def format_prices(data):
  BTC
  - USD: {btc_usd}
  - USDT: {btc_usdt}
+ - PKR: {btc_pkr}
 
  ETH
  - USD: {eth_usd}
  - USDT: {eth_usdt}
+ - PKR: {eth_pkr}
 """
 
     return message.strip()
